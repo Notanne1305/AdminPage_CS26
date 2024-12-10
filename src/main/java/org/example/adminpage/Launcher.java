26127d2ac69f70bd1bca2690b6ef1f0f5cd888e6
Notanne1305 <j.sindo.545117@umindanao.edu.ph>
2024-12-10 11:09:38 +0800

    Log In Cont

--- src/main/java/org/example/adminpage/Launcher.java
+++ src/main/java/org/example/adminpage/Launcher.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,41 +18,16 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        // Save a reference to the primary stage
         this.primaryStage = stage;
 
         // Load the FXML file for the login screen
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LogIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1521, 800);
-
-        // Set up the stage
-        stage.setTitle("Login Page");
+        stage.setTitle("Hello");
         stage.setScene(scene);
         stage.show();
     }
 
-    // Controller-related fields and methods
-    @FXML
-    private Button LogInBtn;
-
-    @FXML
-    void handleLogIn(ActionEvent event) {
-
-        try {
-
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Category.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 1521, 800);
-
-            // Update the primary stage with the new scene
-            primaryStage.setTitle("Category Page");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-            System.out.println("Login button clicked!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/main/java/org/example/adminpage/LogInCont.java
@@ -0,0 +1,64 @@
+package org.example.adminpage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogInCont {
+
+    @FXML
+    private Button LogInBtn;
+
+    @FXML
+    private PasswordField PasswordTF;
+
+    @FXML
+    private TextField UsernameTF;
+
+    @FXML
+    void handleLogIn(ActionEvent event) {
+
+        String username = "admin";
+        String password = "admin";
+
+        if (UsernameTF.getText().equals(username) && PasswordTF.getText().equals(password)) {
+            try {
+
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Category.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 1521, 800);
+
+                // Update the primary stage with the new scene
+                Stage stage = new Stage();
+                stage.setTitle("Category Page");
+                stage.setScene(scene);
+                stage.show();
+
+                Stage window = (Stage) LogInBtn.getScene().getWindow();
+                window.close();
+
+                System.out.println("Login button clicked!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (UsernameTF.getText().isBlank()) {
+            UsernameTF.setStyle("-fx-border-color: red");
+        } else if (PasswordTF.getText().isBlank()) {
+            PasswordTF.setStyle("-fx-border-color: red");
+
+        } else {
+            System.out.println("Invalid username or password!");
+            UsernameTF.setStyle("-fx-border-color: red");
+            PasswordTF.setStyle("-fx-border-color: red");
+        }
+
+    }
+
+    }
+
