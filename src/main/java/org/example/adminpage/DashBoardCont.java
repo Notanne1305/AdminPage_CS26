f08f3d9e1d80cfb4cd755f7245993e0dbccb56e5
elbse <changpriego@gmail.com>
2024-12-13 20:36:48 +0800

    StocksCont Updated

--- src/main/java/org/example/adminpage/DashBoardCont.java
+++ src/main/java/org/example/adminpage/DashBoardCont.java
@@ -59,17 +59,14 @@ public class DashBoardCont {
 
         @FXML
         public void initialize() {
-            // Create a circular clip
-            Circle clip = new Circle();
+                // Set clipping to the ImageView
+                Circle clip = new Circle();
+                clip.setCenterX(imageView.getFitWidth() / 2);
+                clip.setCenterY(imageView.getFitHeight() / 2);
+                clip.setRadius(Math.min(imageView.getFitWidth(), imageView.getFitHeight()) / 2);
 
-            // Set the center and radius of the circle based on ImageView dimensions
-            clip.setCenterX(imageView.getFitWidth() / 2); // Center horizontally
-            clip.setCenterY(imageView.getFitHeight() / 2); // Center vertically
-            clip.setRadius(Math.min(imageView.getFitWidth(), imageView.getFitHeight()) / 2); // Smaller dimension
-
-            // Apply the clip to the ImageView
-            imageView.setClip(clip);
-        }
+                imageView.setClip(clip);
+            }
     }
 
 
