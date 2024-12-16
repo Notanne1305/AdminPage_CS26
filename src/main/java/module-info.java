8127e5de294a936182634dcfca9a9a382c838318
loftyyyy <lofty08.business@gmail.com>
2024-12-16 10:20:01 +0800

    Addded, new database conencectino

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module org.example.adminpage {
     requires com.dlsc.formsfx;
     requires com.jfoenix;
     requires java.desktop;
+    requires java.sql;
+    requires com.zaxxer.hikari;
 
     opens org.example.adminpage to javafx.fxml;
     exports org.example.adminpage;
---
+++ src/main/java/org/example/adminpage/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package org.example.adminpage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/kioskmanagementsystem");
+        config.setUsername("root");
+        config.setPassword("");
+        config.setMaximumPoolSize(10); // Maximum number of connections
+        config.setMinimumIdle(2);     // Minimum idle connections
+        config.setIdleTimeout(30000); // 30 seconds
+        config.setMaxLifetime(1800000); // 30 minutes
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
